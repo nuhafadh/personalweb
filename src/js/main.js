3a9478137fcc7903e710cadf8e9b506adc6aa6d1
Nuha Fadh <fayn.deco@gmail.com>
2023-09-09 15:05:42 +0700

    typed.js

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import Typed from 'typed.js';
+
+new Typed('#heading', {
+    strings: ['Looking for Creative Service for optimizing your Business? I am here to help you __', 'I am Nuha Fadh, a Web Designer & Digital Marketer __', 'I am running creative agency called The Doors! __'],
+    typeSpeed: 15,
+    backSpeed: 45,
+    cursorChar: "",
+    loop: true,
+  });
